f54532249560587f25adbe35c865062d5bc54b8b
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-09-26 16:40:57 +0500

    graduate-work-app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiSearch } from 'react-icons/bi'
 
 const Navbar = () => {
-  return (
-    <div className='Navbar'>
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    Navbar
+    return (
+        <div className='Navbar'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <Link className='logo' to='/'>SCHEMA</Link>
+                    </div>
+                    <div className="col-lg-6">
+                        <ul>
+                            <li><Link to='/'>HOME</Link></li>
+                            <li><Link to='/'>HOME</Link></li>
+                            <li><Link to='/'>HOME</Link></li>
+                            <li><Link to='/'>HOME</Link></li>
+                            <li><Link to='/'>HOME</Link></li>
+                            <li><Link to='/'><BiSearch /></Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
