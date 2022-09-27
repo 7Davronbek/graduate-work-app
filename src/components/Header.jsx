74138fd7e73f8fcb5137d167f49dffb268138c5a
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-09-27 16:04:47 +0500

    graduate-work-app

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-10">
+                    <h1>Over <span>6,500</span> Curated Design Resources, <span>SCHEMA </span> Templates</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
