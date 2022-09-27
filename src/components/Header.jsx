127413ff5d7f55abb28b610d48eca7276a86e774
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-09-27 17:40:04 +0500

    graduate-work-app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div className='Header'>
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-10 mx-auto">
-                    <h1>Over <span>6,500</span> Curated Design Resources, <span>Schema </span> Templates</h1>
+    return (
+        <div className='Header'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h1>Over <span>6,500</span> Curated  Design <br /> Resources, <span>Schema </span> Templates in here</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio est fuga ium quasi omnis accusamus magni deleniti enim reiciendis</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
