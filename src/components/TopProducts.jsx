06f4f10dcb5f9865c475eef50459ce8c4f901175
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-09-28 17:16:45 +0500

    graduate-work-app

---
+++ src/components/TopProducts.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const TopProducts = () => {
+  return (
+    <div className='TopProducts'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>Top Products</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, minus!</p>
+                </div>
+                <div className="col-lg-4">
+                    <div className="cards">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TopProducts
