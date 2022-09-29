f217c9a0fd2c15a31fa333c3a05b535441c70654
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-09-29 13:50:05 +0500

    graduate-work-app

--- src/components/TopProducts.jsx
+++ src/components/TopProducts.jsx
@@ -3,78 +3,78 @@ import { AiOutlineHeart } from 'react-icons/ai'
 import { FiSearch, FiShoppingBag } from 'react-icons/fi'
 
 const TopProducts = () => {
-  return (
-    <div className='TopProducts'>
-        <div className="container">
-            <div className="row">
-                <div className="col-12 text-center">
-                    <h1>Top Products</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, minus!</p>
-                </div>
-                <div className="col-lg-4">
-                    <div className="cards">
-                        <img src="/images/1.png" className='w-100' alt="" />
-                        <div className="btnWrap">
-                            <div className="shop"><FiShoppingBag /></div>
-                            <div className="heart"><AiOutlineHeart /></div>
-                            <div className="search"><FiSearch /></div>
+    return (
+        <div className='TopProducts'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h1>Top Products</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, minus!</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="cards">
+                            <img src="/images/1.png" className='w-100' alt="" />
+                            <div className="btnWrap">
+                                <div className="shop"><FiShoppingBag /></div>
+                                <div className="heart"><AiOutlineHeart /></div>
+                                <div className="search"><FiSearch /></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="cards">
-                        <img src="/images/2.png" className='w-100' alt="" />
-                        <div className="btnWrap">
-                            <div className="shop"><FiShoppingBag /></div>
-                            <div className="heart"><AiOutlineHeart /></div>
-                            <div className="search"><FiSearch /></div>
+                    <div className="col-lg-4">
+                        <div className="cards">
+                            <img src="/images/2.png" className='w-100' alt="" />
+                            <div className="btnWrap">
+                                <div className="shop"><FiShoppingBag /></div>
+                                <div className="heart"><AiOutlineHeart /></div>
+                                <div className="search"><FiSearch /></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="cards">
-                        <img src="/images/3.png" className='w-100' alt="" />
-                        <div className="btnWrap">
-                            <div className="shop"><FiShoppingBag /></div>
-                            <div className="heart"><AiOutlineHeart /></div>
-                            <div className="search"><FiSearch /></div>
+                    <div className="col-lg-4">
+                        <div className="cards">
+                            <img src="/images/3.png" className='w-100' alt="" />
+                            <div className="btnWrap">
+                                <div className="shop"><FiShoppingBag /></div>
+                                <div className="heart"><AiOutlineHeart /></div>
+                                <div className="search"><FiSearch /></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="cards">
-                        <img src="/images/4.png" className='w-100' alt="" />
-                        <div className="btnWrap">
-                            <div className="shop"><FiShoppingBag /></div>
-                            <div className="heart"><AiOutlineHeart /></div>
-                            <div className="search"><FiSearch /></div>
+                    <div className="col-lg-4">
+                        <div className="cards">
+                            <img src="/images/4.png" className='w-100' alt="" />
+                            <div className="btnWrap">
+                                <div className="shop"><FiShoppingBag /></div>
+                                <div className="heart"><AiOutlineHeart /></div>
+                                <div className="search"><FiSearch /></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="cards">
-                        <img src="/images/7.png" className='w-100' alt="" />
-                        <div className="btnWrap">
-                            <div className="shop"><FiShoppingBag /></div>
-                            <div className="heart"><AiOutlineHeart /></div>
-                            <div className="search"><FiSearch /></div>
+                    <div className="col-lg-4">
+                        <div className="cards">
+                            <img src="/images/7.png" className='w-100' alt="" />
+                            <div className="btnWrap">
+                                <div className="shop"><FiShoppingBag /></div>
+                                <div className="heart"><AiOutlineHeart /></div>
+                                <div className="search"><FiSearch /></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="cards">
-                        <img src="/images/6.png" className='w-100' alt="" />
-                        <div className="btnWrap">
-                            <div className="shop"><FiShoppingBag /></div>
-                            <div className="heart"><AiOutlineHeart /></div>
-                            <div className="search"><FiSearch /></div>
+                    <div className="col-lg-4">
+                        <div className="cards">
+                            <img src="/images/6.png" className='w-100' alt="" />
+                            <div className="btnWrap">
+                                <div className="shop"><FiShoppingBag /></div>
+                                <div className="heart"><AiOutlineHeart /></div>
+                                <div className="search"><FiSearch /></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default TopProducts
