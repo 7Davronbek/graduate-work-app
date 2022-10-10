be7a94de70d2b11baae2a0cc7d1b464ee927aa2d
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-10-10 16:12:10 +0500

    graduate-work-app

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,44 +1,56 @@
 
 export const data = [
     {
+        'id': 1,
+        'qty': 0,
         'image': '/images/1.png',
         'title': 'Lorem ipsum dolor sit amet consectetur  elit.',
-        'price': '120.000 sum',
+        'price': '120.000',
         'creator': 'Abdushkurov Shavkat',
         'category': 'Icon'
     },
     {
+        'id': 2,
+        'qty': 0,
         'image': '/images/2.png',
         'title': 'Lorem ipsum dolor sit amet consectetur  elit.',
-        'price': '120.000 sum',
+        'price': '120.000',
         'creator': 'Abdushkurov Shavkat',
         'category': 'Icon'
     },
     {
+        'id': 3,
+        'qty': 0,
         'image': '/images/3.png',
         'title': 'Lorem ipsum dolor sit amet consectetur  elit.',
-        'price': '120.000 sum',
+        'price': '120.000',
         'creator': 'Abdushkurov Shavkat',
         'category': 'Mochups'
     },
     {
+        'id': 4,
+        'qty': 0,
         'image': '/images/4.png',
         'title': 'Lorem ipsum dolor sit amet consectetur  elit.',
-        'price': '120.000 sum',
+        'price': '120.000',
         'creator': 'Abdushkurov Shavkat',
         'category': 'Icon'
     },
     {
+        'id': 5,
+        'qty': 0,
         'image': '/images/7.png',
         'title': 'Lorem ipsum dolor sit amet consectetur  elit.',
-        'price': '120.000 sum',
+        'price': '120.000',
         'creator': 'Abdushkurov Shavkat',
         'category': 'Icon'
     },
     {
+        'id': 6,
+        'qty': 0,
         'image': '/images/6.png',
         'title': 'Lorem ipsum dolor sit amet consectetur  elit.',
-        'price': '120.000 sum',
+        'price': '120.000',
         'creator': 'Abdushkurov Shavkat',
         'category': 'Mochups'
     },
